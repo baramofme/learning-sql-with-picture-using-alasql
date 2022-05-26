bc4ea292b84868699e7fc37d7364ef0ba501b676
jihoon Yi <baramofme@gmail.com>
2022-05-26 18:32:10 +0900

    챕터 3 내용 복습 작성 중

--- loadAll.js
+++ loadAll.js
@@ -2,6 +2,7 @@ const repl = require('repl');
 const initDB = require('./initDB')
 const ji_chap1 = require("./chap1/ji_chap1")
 const ji_chap2 = require("./chap2/ji_chap2")
+const ji_chap3 = require("./chap3/ji_chap3")
 
 const populatedAlasql = initDB()
 
@@ -12,7 +13,8 @@ local.context.alasql = populatedAlasql
 // jihoon
 local.context.ji = {
     chap1: ji_chap1(populatedAlasql),
-    chap2: ji_chap2(populatedAlasql)
+    chap2: ji_chap2(populatedAlasql),
+    chap3: ji_chap3(populatedAlasql),
 }
 // migol
 local.context.mi = {
